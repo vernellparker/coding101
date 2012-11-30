ed610d543b965ea17c99d46ae2588abf4d001d77
vernell parker <vernell.parker@gmail.com>
2012-11-29 20:54:12 -0500

    Updated with changes

---
+++ sdi-project2/parker-vernell-assignment2.js
@@ -0,0 +1,28 @@
+// Vernell A. Parker
+// Project 2
+// SDI 1212
+// Functions and Loops
+
+var drill = "Drill Sergeant";
+var countDown = 10;
+var soldier = ("Miller", "Johnson" , "Perez");
+
+//funcion
+var time = function (x,y) {
+	var weeks = x-y;
+	console.log(weeks +" weeks goes by...") 
+};
+
+var time2 = function (x,y) {
+    var days = x-y;
+	console.log(days + " days until global destruction")
+	return days;
+};
+
+time(10,7);
+console.log(".... They are coming..."); 
+console.log(".... Closer ...")
+console.log("....They came for us...");
+console.log("They called to us over loud speakers glorifying the imminent doom that is about to become of us");
+
+  
