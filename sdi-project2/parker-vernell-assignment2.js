43a632e22900721d89cbc717dee0a594af0c3d22
vernell parker <vernell.parker@gmail.com>
2012-11-29 21:23:33 -0500

    Updated 3

--- sdi-project2/parker-vernell-assignment2.js
+++ sdi-project2/parker-vernell-assignment2.js
@@ -10,19 +10,28 @@ var soldier = ("Miller", "Johnson" , "Perez");
 //funcion
 var time = function (x,y) {
 	var weeks = x-y;
+	if (weeks === 0) { 
+	console.log(" its too late!!!");
+	} else{
 	console.log(weeks +" weeks goes by...") 
+	};
 };
 
 var time2 = function (x,y) {
     var days = x-y;
-	console.log(days + " days until global destruction")
 	return days;
 };
+//main code
 
 time(10,7);
 console.log(".... They are coming..."); 
-console.log(".... Closer ...")
+console.log(".... Closer ...");
 console.log("....They came for us...");
 console.log("They called to us over loud speakers glorifying the imminent doom that is about to become of us");
 
-  
+var minusDays = time2(100,98);
+console.log(minusDays + " days until global destruction");
+minusDays = time2 (59,58);
+console.log(minusDays + " day until global destruction");
+
+console.log("But " + drill + " has an idea that may save the world"); 
