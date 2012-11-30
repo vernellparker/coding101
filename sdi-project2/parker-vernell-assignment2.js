035b5eba8d9ba60e7742df8e3cea71a50326d3a7
vernell parker <vernell.parker@gmail.com>
2012-11-29 22:15:13 -0500

    Update 4

--- sdi-project2/parker-vernell-assignment2.js
+++ sdi-project2/parker-vernell-assignment2.js
@@ -29,9 +29,13 @@ console.log(".... Closer ...");
 console.log("....They came for us...");
 console.log("They called to us over loud speakers glorifying the imminent doom that is about to become of us");
 
-var minusDays = time2(100,98);
-console.log(minusDays + " days until global destruction");
-minusDays = time2 (59,58);
-console.log(minusDays + " day until global destruction");
-
+days = time2(100,98);
+console.log(days + " days until global destruction");
+days = time2 (59,58);
+console.log(days + " day until global destruction");
+if (days === 0) {
+	console.log("We need more time");
+	} else { ("");
+};
 console.log("But " + drill + " has an idea that may save the world"); 
+
