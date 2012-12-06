e4bf27db913b25d545f921327505cf0f547fb00f
vernell parker <vernell.parker@gmail.com>
2012-12-06 15:49:15 -0500

    Update2

--- sdi-project3/parker-vernell-assignment3.js
+++ sdi-project3/parker-vernell-assignment3.js
@@ -5,16 +5,34 @@
 var drill = "Drill Sergeant";
 var soldier = ["Miller", "Johnson", "Perez"];
 var gear = ["rifle", "handheld", "grenade!!!"];
-var warCry = ("Let them come" + "We kill them all!!!!");
-
+var warCry = ("Let them come. " + "We'll kill them all!!!!");
+var theMoment = ("It is time to" + " rid ourselves of this alien menus!!!" )
 var ship = {
 	name: "Death Star",
-	race: "Alien",
+	race: "Glormac",
 	size: " 100 miles long",
 	threat: " Immediate Danger",
 };
 
+
+
 ship.name = "Destoryer";
 
 console.log("A new ship known as the " + ship.name + " has been spotted above the skys.");
- 	
+console.log(drill + " said, " + warCry);
+console.log(theMoment); 	
+console.log("Open your handheld and get me the the statics on the male and female sexes of the Glormacs'. Find me there weaknesses!!!!") 
+
+var handheldScreen = {
+"Glormac": [
+	{
+		"Sex":        "Male",
+		"Weaknesses": "Skin: vulnerable to extreme heat",
+	},
+	{
+		"Sex":		  "Female",
+		"Weaknesses": "Skin porous: vulnerable to high levels of smoke"
+	}
+  ]
+}
+console.log(handheldScreen);
