3cc6dd7cce22a6f47ca3bb15dd12b6e5e1188a7d
vernell parker <vernell.parker@gmail.com>
2012-12-05 23:32:34 -0500

    Update 1

---
+++ sdi-project3/parker-vernell-assignment3.js
@@ -0,0 +1,20 @@
+// Vernell Parker
+// SDI 1212
+// Project 3
+
+var drill = "Drill Sergeant";
+var soldier = ["Miller", "Johnson", "Perez"];
+var gear = ["rifle", "handheld", "grenade!!!"];
+var warCry = ("Let them come" + "We kill them all!!!!");
+
+var ship = {
+	name: "Death Star",
+	race: "Alien",
+	size: " 100 miles long",
+	threat: " Immediate Danger",
+};
+
+ship.name = "Destoryer";
+
+console.log("A new ship known as the " + ship.name + " has been spotted above the skys.");
+ 	
