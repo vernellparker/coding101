a414c319d4b51428590ab1acca90de7b78affba2
vernell parker <vernell.parker@gmail.com>
2012-12-06 19:55:05 -0500

    Update3

--- sdi-project3/parker-vernell-assignment3.js
+++ sdi-project3/parker-vernell-assignment3.js
@@ -7,6 +7,8 @@ var soldier = ["Miller", "Johnson", "Perez"];
 var gear = ["rifle", "handheld", "grenade!!!"];
 var warCry = ("Let them come. " + "We'll kill them all!!!!");
 var theMoment = ("It is time to" + " rid ourselves of this alien menus!!!" )
+var distance = 150
+var ready = true
 var ship = {
 	name: "Death Star",
 	race: "Glormac",
@@ -21,7 +23,7 @@ ship.name = "Destoryer";
 console.log("A new ship known as the " + ship.name + " has been spotted above the skys.");
 console.log(drill + " said, " + warCry);
 console.log(theMoment); 	
-console.log("Open your handheld and get me the the statics on the male and female sexes of the Glormacs'. Find me there weaknesses!!!!") 
+console.log("Open your " + gear[1] + " and get me the the statics on the male and female sexes of the Glormacs'. Find me there weaknesses!!!!") 
 
 var handheldScreen = {
 "Glormac": [
@@ -35,4 +37,14 @@ var handheldScreen = {
 	}
   ]
 }
-console.log(handheldScreen);
+
+console.log(handheldScreen)
+console.log("We have the information we need.");
+console.log("How far away is the enemy?")
+console.log(distance + " miles away")
+if (distance >= 150){
+	console.log("We have plenty of time to put our plan in place!!!")
+	} else { console.log(" We are running out of time")
+	};
+	
+	
