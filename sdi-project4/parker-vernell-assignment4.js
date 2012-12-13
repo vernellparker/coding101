93d124d79c449b27a765344e52a1d8b887c7c079
vernell parker <vernell.parker@gmail.com>
2012-12-12 19:19:53 -0500

    Updated 1

---
+++ sdi-project4/parker-vernell-assignment4.js
@@ -0,0 +1,46 @@
+/* Vernell Parker
+   SDI 1212
+   Project 4
+   Code Library
+*/
+
+var myLibrary = function(){
+
+//  String for phone number Pattern
+
+
+//  String for email address
+
+
+//  String start with http: or https:
+
+
+//  Title-case a string
+
+
+//  String return separator
+
+
+//  Decimals Places 
+
+
+//  Fuzzy- match a number
+
+
+//  Number of hours or days
+
+
+//  Return Actual Number
+
+
+//  Smallest value in array
+
+
+//  Total value in an array
+
+
+//  Return array sorted by the value
+
+
+
+};
