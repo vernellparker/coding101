12016e1f7eed40a12e991be31c60210fccf85b06
vernell parker <vernell.parker@gmail.com>
2012-12-13 18:05:52 -0500

    update 2

--- sdi-project4/parker-vernell-assignment4.js
+++ sdi-project4/parker-vernell-assignment4.js
@@ -7,12 +7,16 @@
 var myLibrary = function(){
 
 //  String for phone number Pattern
+	
 
 
 //  String for email address
+	
+			  
 
 
 //  String start with http: or https:
+
 
 
 //  Title-case a string
@@ -32,15 +36,33 @@ var myLibrary = function(){
 
 //  Return Actual Number
 
+	
 
 //  Smallest value in array
-
+Array.prototype.minGreaterThan = function(numValue){
+ 
+  var num=Number.POSITIVE_INFINITY,
+  i;
+ 
+    for (i=0;i<this.length;i++) if (numValue<this[i] && this[i]<num) num=this[i];
+    return num;
+};
 
 //  Total value in an array
 
 
 //  Return array sorted by the value
 
+	return {
+		"num": num
+	
+	}
 
 
 };
+
+
+var newLib = new myLibrary();
+
+var arr=[15,45,81,64,125,792,458,1657,152,154,20],num=10;
+console.log(arr.minGreaterThan(num));
